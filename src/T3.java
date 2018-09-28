987be95b07fac004d925176a3961bea67777d442
rina Usoltceva <irina.710@mail.ru>
2018-09-28 10:21:56 +0300

    В Т3 доделала переименовку файлов

--- src/T3.java
+++ src/T3.java
@@ -9,22 +9,22 @@ public class T3 {
 
     private static void task3() {
         List<String> fileNames = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h");
-
         try {
-            /*
-           Path source = ...
-           Files.move(source, source.resolveSibling("newname"));
-            */
-            Files.move("a.txt", "1 a.txt", StandardCopyOption.REPLACE_EXISTING);
+            for (String fileName : fileNames) {
+                Path old = Paths.get("t3_katalog\\\\" + fileName + ".txt");
+                Path updated = Paths.get("t3_katalog\\\\" + String.valueOf(fileNames.indexOf(fileName) + 1) + " " + fileName + ".txt");
+
+                //для возвращения без номеров
+                //Path old = Paths.get("t3_katalog\\\\" + String.valueOf(fileNames.indexOf(fileName) + 1) + " " + fileName + ".txt");
+                //Path updated = Paths.get("t3_katalog\\\\" + fileName + ".txt");
+
+                Files.move(old, updated, StandardCopyOption.REPLACE_EXISTING);
+            }
         } catch (Exception e) {
             System.out.println("Ошибка");
         }
 
         /*
-            Дан список имен файлов.
-            Предполагается, что эти файлы лежат в одном заранее выбранном вами каталоге.
-
-            Нужно переименовать каждый файл так, чтобы в начало к нему был приписан номер файла в списке.
             Например, если был список a.txt, b.txt, то нунжо переименовать файлы в 1 a.txt и 2 b.txt.
             Для каждого файла выведите, удалось ли его переименовать, а если нет, то что именно пошло не так.
             Проследите, чтобы не выводилось лишней информации об ошибках наподобие содержимого стека.
