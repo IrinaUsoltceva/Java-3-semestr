99447fbbae76ad97ddebabe72f69151abb3e77b0
rina Usoltceva <irina.710@mail.ru>
2018-09-28 10:34:42 +0300

    Т3: удачно/неудачно переименовали

--- src/T3.java
+++ src/T3.java
@@ -8,31 +8,35 @@ public class T3 {
     }
 
     private static void task3() {
-        List<String> fileNames = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h");
+        List<String> fileNames = Arrays.asList("a.txt", "b.txt", "c.txt", "d.txt", "e.txt", "f.txt", "g.txt", "h.txt");
+        String catalogName = "t3_katalog";
+
         try {
             for (String fileName : fileNames) {
-                Path old = Paths.get("t3_katalog\\\\" + fileName + ".txt");
-                Path updated = Paths.get("t3_katalog\\\\" + String.valueOf(fileNames.indexOf(fileName) + 1) + " " + fileName + ".txt");
+                Path old = Paths.get(catalogName + "\\\\" + fileName);
+                String newFileName = String.valueOf(fileNames.indexOf(fileName) + 1) + " " + fileName;
+                Path updated = Paths.get(catalogName  + "\\\\" + newFileName);
 
                 //для возвращения без номеров
-                //Path old = Paths.get("t3_katalog\\\\" + String.valueOf(fileNames.indexOf(fileName) + 1) + " " + fileName + ".txt");
-                //Path updated = Paths.get("t3_katalog\\\\" + fileName + ".txt");
+                //Path old = Paths.get("t3_katalog\\\\" + String.valueOf(fileNames.indexOf(fileName) + 1) + " " + fileName);
+                //Path updated = Paths.get("t3_katalog\\\\" + fileName);
 
                 Files.move(old, updated, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println(fileName + " Успешно переименован в " + newFileName);
             }
         } catch (Exception e) {
             System.out.println("Ошибка");
+            System.out.println("UnsupportedOperationException – if the array contains a copy option that is not supported, " +
+                            "java.nio.file.FileAlreadyExistsException – if the target file exists but cannot be replaced because the REPLACE_EXISTING option is not specified (optional specific exception)" +
+                            "java.nio.file.DirectoryNotEmptyException – the REPLACE_EXISTING option is specified but the file cannot be replaced because it is a non-empty directory (optional specific exception)" +
+                            "java.nio.file.AtomicMoveNotSupportedException – if the options array contains the ATOMIC_MOVE option but the file cannot be moved as an atomic file system operation." +
+                            "java.io.IOException – if an I/O error occurs" +
+                            "SecurityException – In the case of the default provider, and a security manager is installed, the checkWrite method is invoked to check write access to both the source and target file);");
         }
 
         /*
-            Например, если был список a.txt, b.txt, то нунжо переименовать файлы в 1 a.txt и 2 b.txt.
             Для каждого файла выведите, удалось ли его переименовать, а если нет, то что именно пошло не так.
             Проследите, чтобы не выводилось лишней информации об ошибках наподобие содержимого стека.
-            Используйте метод Files.move().
-
-            Files.move(source, dest, ...)
-                    :Path  :Path ещо опции
-                                StandartCopyOption.REPLACE_EXISTING
 
          */
     }
