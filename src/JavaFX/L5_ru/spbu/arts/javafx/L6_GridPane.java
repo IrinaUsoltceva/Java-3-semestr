a3c1e33eb6a2b5694864665788d6f344fbd60899
Irina Usoltceva <irina.710@mai;.ru>
2018-10-12 11:38:15 +0300

    l6

--- src/JavaFX/L5_ru/spbu/arts/javafx/L6_GridPane.java
+++ src/JavaFX/L5_ru/spbu/arts/javafx/L6_GridPane.java
@@ -10,7 +10,6 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
-
 public class L6_GridPane extends Application {
     @Override
     public void start(Stage primaryStage) {
@@ -23,49 +22,56 @@ public class L6_GridPane extends Application {
     }
 
     private Parent initInterface() {
-        //реализуем интерфейс месседжера с помощью GridPane
-        //начнем с создания элементов интерфейса
+        //реализуем интерфейс мессенджера с помощью GridPane
 
+        //начнем с создания элементов интерфейса
         TextArea history = new TextArea();
         TextField message = new TextField();
-        Button send = new Button("send");
-        Label contactsLabel = new Label("контакты");
+        Button send = new Button("Send");
+        Label contactsLabel = new Label("Contacts");
         ListView<String> contacts = new ListView<>();
         //ListView имеет типовый параметр, тип содержимого списка
 
         GridPane root = new GridPane();
 
-        //констраинты для элементов можно указать традиционно
-        GridPane.setColumnIndex(history, 0); //в каком столбце элемент
-        GridPane.setRowIndex(history, 0); //в какой строка
-        GridPane.setColumnSpan(history, 2);//сколько занимает
-        GridPane.setRowSpan(history, 2);//сколько строк
+        //constraint для элементов можно указать традиционно.
+        //номера строк и столбцов считаются с 0
+        GridPane.setColumnIndex(history, 0); //в каком столбце
+        GridPane.setRowIndex(history, 0); //в какой строке
+        GridPane.setColumnSpan(history, 2); //сколько столбцов
+        GridPane.setRowSpan(history, 2); //сколько строк
         root.getChildren().add(history);
 
-        //но можно делать проще
+        //можно делать проще
         root.add(message, 0, 2);
-        root.add(send,1,2);
-        root.add(contactsLabel,2,0);
-        root.add(contacts,2,1,1,2);
+        root.add(send, 1, 2);
+        root.add(contactsLabel, 2, 0);
+        root.add(contacts, 2, 1, 1, 2);
         //для contacts дополнительно указали количество строк и столбцов
 
-        //заставим элементы растягиваться. нужно указать, как растягиваются строки и столбцы.
-
-        //нужно указать, как растягиваются строки и столбцы.
-        //будем растягивать столбец 0 и строку 1
+        //заставим элементы растягиваться.
+        //Нужно указывать, как растягиваются
+        //строки и столбцы. Будем растягивать столбец 0
+        //и строку 1
 
         ColumnConstraints cc0 = new ColumnConstraints();
         cc0.setHgrow(Priority.ALWAYS);
-        //HGrow аналогичен HGrow для HBox
+        //Hgrow аналогичен Hgrow для HBox
         //есть много других полезных constraint в cc0
 
         RowConstraints rc0 = new RowConstraints();
         RowConstraints rc1 = new RowConstraints();
         rc1.setVgrow(Priority.ALWAYS);
 
-        //column/riw constraints - изначально пустые списки
+        //column/row constraints - это изначально
+        //пустые списки.
         root.getColumnConstraints().add(cc0);
         root.getRowConstraints().addAll(rc0, rc1);
+
+        //список контактов нужно делать не очень высоким,
+        //потому что иначе в последняя строка тоже
+        //становится высокой
+        contacts.setPrefSize(200, 0);
 
         return root;
     }
