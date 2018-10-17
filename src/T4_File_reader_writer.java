8e37a482d0057d29fe58c5694841729bb9015f5e
Irina Usoltceva <irina.710@mail.ru>
2018-10-17 21:36:55 +0300

    Т4

--- src/T4_File_reader_writer.java
+++ src/T4_File_reader_writer.java
@@ -34,58 +34,53 @@ public class T4_File_reader_writer {
     private static void task4_3() {
 
     //utf8
-        try {
+        try (Writer out = new OutputStreamWriter(new FileOutputStream("text_utf8.bin"),"UTF8")) {
             Path pathText_utf8 = Paths.get("text_utf8.txt");
-            //text_utf8.
-            byte[] text_utf8 = {-48, -89, -48, -75, -47, -126, -47,
-                    -117,
-                    -47,
-                    -128,
-                    -48,
-                    -75,
-                    32,
-                            -47,
-                            -121,
-                            -47};
-            //byte[] text_utf8 = Files.readAllBytes(pathText_utf8);
-            for (int i = 0; i < text_utf8.length; i++) {
-                System.out.println(text_utf8[i]);
+            byte[] text_utf8 = Files.readAllBytes(pathText_utf8);
+
+            for (byte aText_utf8 : text_utf8) {
+                int bb = aText_utf8 < 0 ? 256 + aText_utf8 : aText_utf8;
+                String bb16 = Integer.toString(bb, 16);
+                out.write(bb16 + " ");
             }
-
-            //доп кодом
-            Files.write(Paths.get("text_utf8_b.bin"), text_utf8);
-            //в число
-
-            //16ричное
-
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+    //1251
+        try (Writer out = new OutputStreamWriter(new FileOutputStream("text_win1251.bin"),"windows-1251")) {
+            Path pathText_utf8 = Paths.get("text_win1251.txt");
+            byte[] text_utf8 = Files.readAllBytes(pathText_utf8);
+
+            for (byte aText_utf8 : text_utf8) {
+                int bb = aText_utf8 < 0 ? 256 + aText_utf8 : aText_utf8;
+                String bb16 = Integer.toString(bb, 16);
+                out.write(bb16 + " ");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    //koir
+        try (Writer out = new OutputStreamWriter(new FileOutputStream("text_koi8r.bin"),"KOI8_R")) {
+            Path pathText_utf8 = Paths.get("text_koi8r.txt");
+            byte[] text_utf8 = Files.readAllBytes(pathText_utf8);
+
+            for (byte aText_utf8 : text_utf8) {
+                int bb = aText_utf8 < 0 ? 256 + aText_utf8 : aText_utf8;
+                String bb16 = Integer.toString(bb, 16);
+                out.write(bb16 + " ");
+            }
+
+            for (byte aText_utf8 : text_utf8)
+                aText_utf8 = aText_utf8 < 0 ? (byte) (128 + aText_utf8) : aText_utf8;
+            Files.write(Paths.get("text_koi7r.txt"), text_utf8);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
-    /*4.3
-        любой файл состоит из байтов - восьмибитные кусочик информации
-        8 нолей/единичек
-        кодировки превращат байты в буквы
-        ютф8 для русских букв по два байта
-        нужно вывести байты
 
-        при чтении байт из файла получаем тип byte
-
-        как мы будем это в файл выводить
-        есть byte b
-        как написать?
-            - можно просто распечатать - получу дополнительный код
-            - можно превратить в обычное число
-                int bb = b < 0? 256 + b  : b
-                шо такое ?
-                это String s = x == 2 ? "два" : "не два"
-                int bb = b&0xFF;
-
-                sout (Integer.toString (bb, 16));
-
-                печатать все три варианта b, bb, bb16
-                со знаком, без знака и в шестнадцатеричной
-     */
 
 }
