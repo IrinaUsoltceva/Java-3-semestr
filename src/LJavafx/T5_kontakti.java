eb2582ba8d567b3c5882572d01beb8b2c1574299
Irina Usoltceva <irina.710@mail.ru>
2018-10-29 15:17:32 +0300

    Т5, изменяю контакты

--- src/LJavafx/T5_kontakti.java
+++ src/LJavafx/T5_kontakti.java
@@ -2,6 +2,8 @@ package LJavafx;
 
 import javafx.application.Application;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -20,6 +22,13 @@ public class T5_kontakti extends Application {
     конпки закрытия окна, бордюр для изменения размеров окна и тд
 
     */
+    private TextArea dialog = new TextArea();
+    private Button send = new Button("send");
+    private TextField message = new TextField();
+    private Label kontakti = new Label("kontakti");
+    private ListView<String> kontaktiList = new ListView<>();
+
+
     @Override
     public void start(Stage primaryStage) {
         //установить заголовок окна
@@ -30,31 +39,21 @@ public class T5_kontakti extends Application {
         //показывать на экране.  И информацию о том, как показывать.
         //Это размер окна и другие параметры
         primaryStage.setScene(new Scene(root/*, width, height*/));
+        initInteraction();
 
         //отобразить окно на экране
         primaryStage.show();
     }
 
     private Parent initInterface() {
-        //В конструкторе можно сразу указать детей, т.е. все подузлы.
-        //И еще можно сразу указать внешний отступ (padding из css)
         HBox root = new HBox();
-
-        TextArea dialog = new TextArea("dialog");
-
-        HBox textAndButton = new HBox();
-        Button send = new Button("send");
-        TextField message = new TextField("message");
+        HBox textAndButton = new HBox(message, send);
         HBox.setHgrow(message, Priority.ALWAYS);
-        textAndButton.getChildren().addAll(message, send);
 
-        VBox left = new VBox();
-        left.getChildren().addAll(dialog, textAndButton);
+        kontaktiList.getItems().addAll("mom", "dad", "sis", "grandma", "jane");
 
-        Label kontakti = new Label("kontakti");
-        ListView<String> kontaktiList = new ListView<>();
-        VBox right = new VBox();
-        right.getChildren().addAll(kontakti, kontaktiList);
+        VBox left = new VBox(dialog, textAndButton);
+        VBox right = new VBox(kontakti, kontaktiList);
 
         root.getChildren().addAll(left, right);
 
@@ -62,23 +61,18 @@ public class T5_kontakti extends Application {
         VBox.setVgrow(dialog, Priority.ALWAYS);
         HBox.setHgrow(left, Priority.ALWAYS);
 
-        /*
-
-        //давайте теперь добавим Constraint для элементов
-        //было бы естественно сделать так
-        //b1.setConstraint (какой, значение)
-
-        //HBox - означает, что констрейнт понимают все HBox-ы
-        //HGrow - название constraint, это тот, который говорит, растягивать
-        //        ли элемент. Always - всегда
-        HBox.setHgrow(tf1, Priority.ALWAYS);
-        HBox.setMargin(b1, new Insets(8));
-
-        //а это уже не constraint, а свойства HBox,
-        //все элементы должны располагаться снизу по центру
-        root.setAlignment(Pos.BOTTOM_CENTER);*/
-
         return root;
     }
 
+    private void initInteraction() {
+        send.addEventHandler(
+                ActionEvent.ACTION,
+                a -> {
+                    dialog.appendText(message.getText() + "\n");
+                    message.textProperty().setValue("");
+                }
+        );
+
+
+    }
 }
