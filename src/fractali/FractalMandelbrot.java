a202eeb3126c41b3ad3160ea4a0c095d97cfb718
Irina Usoltceva <irina.710@mai;.ru>
2018-11-30 12:28:40 +0300

    fractal начало мандельброта

--- src/fractali/FractalMandelbrot.java
+++ src/fractali/FractalMandelbrot.java
@@ -3,10 +3,16 @@ package fractali;
 public class FractalMandelbrot implements Fractal {
     @Override
     public double getColor(double x, double y) {
-        //c = x + i * y
+        //c = p + i * q
         //z_0 = 0
         //z_n+1 = z_n^2 + c
         //сделать N=1000 итераций. На той итерации, когда |z_k| > R = 1e9
         // вернуть k / N
+
+        for (int i = 0; i < 1000; i++) {
+            x =
+        }
+
+        return 1;
     }
 }
