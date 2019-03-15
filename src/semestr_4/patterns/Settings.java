f5d9b86aeb537bc1450e47dda1e0168eecba39f5
Irina Usoltceva <irina.710@mail.ru>
2019-03-15 12:01:55 +0300

    L4, синглтон

--- src/semestr_4/patterns/Settings.java
+++ src/semestr_4/patterns/Settings.java
@@ -1,4 +1,26 @@
 package semestr_4.patterns;
 
+import javafx.scene.paint.Color;
+
 public class Settings {
+
+    //в этом поле хранится тот самый единственный объект класса Settings
+    private static Settings instance = new Settings();
+
+    public static Settings getInstance() {
+        return instance;
+    }
+
+    private Settings() {
+        //загрущить настройки. неважно как, например, из файла, из интернета, из БД
+    }
+
+    //пример полезной информации, содержащейся в сиглтоне
+    public Color getBackground() {
+        return Color.RED;
+    }
+
+    public String getUserName() {
+        return "me";
+    }
 }
