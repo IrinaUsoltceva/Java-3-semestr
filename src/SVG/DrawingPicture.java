756b872cf41795e31afbd0347c4fa175ef6a9ddf
Irina Usoltceva <irina.710@mail.ru>
2019-04-26 10:08:00 +0300

    все до п.11, rand_seed

--- src/SVG/DrawingPicture.java
+++ src/SVG/DrawingPicture.java
@@ -6,9 +6,16 @@ public class DrawingPicture {
     public static void main(String[] args) {
         try (SVG svg = new SVG("a.svg",
                                 Settings.getInstance().getWidth(),
-                                Settings.getInstance().getHeight())) {
+                                Settings.getInstance().getHeight())
+            ) {
 
-            Random random = new Random();
+            Random random;
+            long randSeed = Settings.getInstance().getRandSeed();
+            if (randSeed != 0)
+                random = new Random(Settings.getInstance().getRandSeed());
+            else
+                random = new Random();
+            System.out.println(Settings.getInstance().getRandSeed());
 
             for (int i = 0; i < 1000; i++) {
                 PositionedShape f = new PositionedShape(new SmallSquare(), random.nextInt(400), random.nextInt(400));
--- src/SVG/Settings.java
+++ src/SVG/Settings.java
@@ -9,6 +9,10 @@ import java.util.Properties;
 public class Settings {
     private static Settings instance = new Settings();
     private static Properties p = new Properties();
+    private static int width;
+    private static int height;
+    private static String background;
+    private static String randSeed;
 
     public static Settings getInstance() {
         try {
@@ -16,22 +20,35 @@ public class Settings {
                     new FileInputStream("svg.properties"),
                     StandardCharsets.UTF_8
             ));
+
+            width = Integer.parseInt(p.getProperty("width"));
+            height = Integer.parseInt(p.getProperty("height"));
+            background = p.getProperty("background");
+            randSeed = p.getProperty("rand_seed");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         return instance;
     }
 
-    //сделать обработку ошибок
     public int getWidth() {
-        return Integer.parseInt(p.getProperty("width"));
+        return width;
     }
 
     public int getHeight() {
-        return Integer.parseInt(p.getProperty("height"));
+        return height;
     }
 
     public String getBackground() {
-        return p.getProperty("background");
+        return background;
+    }
+
+    public long getRandSeed () {
+        try {
+            return Integer.parseInt(randSeed);
+        } catch (Exception e) {
+            return 0;
+        }
     }
 }
